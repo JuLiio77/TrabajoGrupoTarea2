4d1afe83df1c57c1afe6ef5169f33146b96ad86b
JuLioVera <92015093+JuLiio77@users.noreply.github.com>
2024-02-16 19:04:05 +0100

    subir service

--- tarea2-back/src/main/java/com/example/tarea2back/services/ClienteService.java
+++ tarea2-back/src/main/java/com/example/tarea2back/services/ClienteService.java
@@ -1,10 +1,23 @@
 package com.example.tarea2back.services;
 
+import com.example.tarea2back.model.ClienteModel;
+import com.example.tarea2back.repository.ClientesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClienteService {
 
+    private ClientesRepository clientesRepository;
+
+    public ClienteModel saveClientes(ClienteModel cliente){
+        return clientesRepository.save(cliente);
+    }
+
+    public List<ClienteModel> listCliente() {
+        return clientesRepository.findAll();
+    }
 
 }
