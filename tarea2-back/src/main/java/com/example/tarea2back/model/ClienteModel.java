a7ec6af6a23aba32b5368579e22c37a0ccd4d441
JoseJavier21 <92015037+JoseJavier21@users.noreply.github.com>
2024-02-16 19:20:32 +0100

    controller

--- tarea2-back/src/main/java/com/example/tarea2back/model/ClienteModel.java
+++ tarea2-back/src/main/java/com/example/tarea2back/model/ClienteModel.java
@@ -1,9 +1,13 @@
 package com.example.tarea2back.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "cliente")
+@Builder
 public class ClienteModel {
 
     @Id
@@ -12,4 +16,46 @@ public class ClienteModel {
     private String nombre;
     private String telefono;
     private String email;
+
+    public ClienteModel() {
+    }
+
+    public ClienteModel(Long id, String nombre, String telefono, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
