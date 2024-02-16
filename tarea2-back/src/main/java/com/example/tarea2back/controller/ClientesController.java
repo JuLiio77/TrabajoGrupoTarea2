27272984d0c1432bbe31cb4aa5a523b97c107e58
JuLioVera <92015093+JuLiio77@users.noreply.github.com>
2024-02-16 18:18:10 +0100

    crear carpetas

---
+++ tarea2-back/src/main/java/com/example/tarea2back/controller/ClientesController.java
@@ -0,0 +1,4 @@
+package com.example.tarea2back.controller;
+
+public class ClientesController {
+}
---
+++ tarea2-back/src/main/java/com/example/tarea2back/repository/ClientesRepository.java
@@ -0,0 +1,4 @@
+package com.example.tarea2back.repository;
+
+public class ClientesRepository {
+}
