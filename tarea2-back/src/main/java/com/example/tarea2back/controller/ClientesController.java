611628abcae515967a17aa613b66f61d7e77b896
JoseJavier21 <92015037+JoseJavier21@users.noreply.github.com>
2024-02-16 19:04:01 +0100

    controller

--- tarea2-back/src/main/java/com/example/tarea2back/controller/ClientesController.java
+++ tarea2-back/src/main/java/com/example/tarea2back/controller/ClientesController.java
@@ -1,4 +1,27 @@
 package com.example.tarea2back.controller;
 
+import com.example.tarea2back.model.ClienteModel;
+import com.example.tarea2back.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/cliente")
 public class ClientesController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping("/guardar")
+    public List<ClienteModel> listCLientes(){
+        return clienteService.listClientes();
+    }
+
+    @PostMapping("/listar")
+    public ClienteModel guardarCliente(){
+        return clienteService.saveCliente();
+    }
 }
