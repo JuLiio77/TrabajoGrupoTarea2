54ded79b692a0c430bc53d071010a8364be59a98
JuLioVera <92015093+JuLiio77@users.noreply.github.com>
2024-02-16 19:27:24 +0100

    cambira para que salga el lisatd

--- tarea2-back/src/main/java/com/example/tarea2back/model/ClienteModel.java
+++ tarea2-back/src/main/java/com/example/tarea2back/model/ClienteModel.java
@@ -12,4 +12,49 @@ public class ClienteModel {
     private String nombre;
     private String telefono;
     private String email;
+
+    public ClienteModel(Long id, String nombre, String telefono, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+
+
+    }
+
+    public ClienteModel() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
