73a87b6477810621c1b3f26d8bb4268af50b0c12
JuLioVera <92015093+JuLiio77@users.noreply.github.com>
2024-02-16 18:05:14 +0100

    subir backend

---
+++ tarea2-back/src/main/java/com/example/tarea2back/Tarea2BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.tarea2back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2BackApplication.class, args);
+	}
+
+}
