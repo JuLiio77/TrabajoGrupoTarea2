8f0495212b8c499553b12fae5fe05342c5830ab0
JoseJavier21 <92015037+JoseJavier21@users.noreply.github.com>
2024-02-16 19:32:18 +0100

    arregar foritn

--- tarea2-front/src/app/cliente.service.ts
+++ tarea2-front/src/app/cliente.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
-  private apiUrl = 'http://localhost:8080/api/clientes'
+  private apiUrl:string = 'http://localhost:8080/api/clientes'
+  private httpHeaders: HttpHeaders = new HttpHeaders({'Content-type' : 'application/json'})
 
   constructor(private http: HttpClient) { }
 
   obtenerClientes(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<any[]>(this.apiUrl + "/list");
   }
 
   agregarCliente(nuevoCliente: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, nuevoCliente);
+    return this.http.post<any>(this.apiUrl, nuevoCliente, {headers: this.httpHeaders});
   }
 }
